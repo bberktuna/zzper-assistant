ee40b2f8e010264a9ae3e464e30b653037ee006a
bberktuna <222bolivez4@gmail.com>
2022-09-04 15:47:04 +0200

    Initial Commit

---
+++ zzper-assistant/zzper_assistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  zzper_assistantApp.swift
+//  zzper-assistant
+//
+//  Created by berk tuna on 04/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct zzper_assistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
